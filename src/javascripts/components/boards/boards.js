5bff1122e5d65a2bb6ec340babf52bfbea1292f9
Heath JH Moore <heathjhmoore@gmail.com>
2019-05-14 20:08:24 -0500

    got event listeners working

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,10 +1,40 @@
 import getData from '../../helpers/data/boardsData';
+import util from '../../helpers/util';
+
+const seePinDiv = (e) => {
+  const boardId = e.target.closest('.card').id;
+  console.error('you clicked a button', boardId);
+  document.getElementById('boards-page').classList.add('hide');
+  document.getElementById('pins-page').classList.remove('hide');
+};
+
+const bindEvents = () => {
+  const allButtons = document.getElementsByClassName('see-pins');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seePinDiv);
+  }
+};
+
+const domBuilder = (array) => {
+  let domString = '';
+  array.forEach((board) => {
+    domString += '<div class="boards col-3">';
+    domString += `<div class="card mt-2 p-2" id="${board.id}">`;
+    domString += `<h4>${board.name}</h4>`;
+    domString += '<button class="btn btn-warning see-pins m-auto col-6">Pins</button>';
+    domString += '</div>';
+    domString += '</div>';
+  });
+  util.printToDom('user-boards', domString);
+  bindEvents();
+};
 
 const initBoards = () => {
   getData.loadBoards()
     .then((resp) => {
       console.error('resp', resp.data.boards);
-      // const { boards } = resp.data;
+      const { boards } = resp.data;
+      domBuilder(boards);
     })
     .catch(err => console.error('error from load boards', err));
 };
